518d66ae6ebe74e676114ca9569d227f8532025c
Peter Hastie <pete@sillypog.com>
2013-12-03 23:43:43 -0800

    Added fixtures for testing.
    Moved sub-tasks to dependencies as they need to be downloaded.

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,25 +30,36 @@ module.exports = function (grunt) {
     clean: {
       tests: ['tmp']
     },
+	
+	env : {
+	  dev : {
+		NODE_ENV : 'DEVELOPMENT'
+	  },
+	  prod : {
+		NODE_ENV : 'PRODUCTION'
+	  }
+	},
 
     // Configuration to be run (and then tested).
     asset_packager: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      }
+	  dev: {
+		  options: {
+			  index: 'test/fixtures/index.html',
+			  dest: 'tmp/dev'
+		  },
+		  files: {
+			  src: 'test/fixtures/asset_packages/**/*.pkg'
+		  }
+	  },
+	  prod: {
+		  options: {
+			  index: 'test/fixtures/index.html',
+			  dest: 'tmp/prod'
+		  },
+		  files: {
+			  src: 'test/fixtures/asset_packages/**/*.pkg'
+		  }
+	  }
     },
 
     // Unit tests.
@@ -63,7 +74,9 @@ module.exports = function (grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'asset_packager', 'nodeunit']);
+  grunt.registerTask('dev', ['env:dev', 'asset_packager:dev']);
+  grunt.registerTask('prod', ['env:prod', 'asset_packager:prod']);
+  grunt.registerTask('test', ['clean', 'dev', 'prod', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
