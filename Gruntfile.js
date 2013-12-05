9c4197d934cf46cbd328e4ab45b702732f0845e2
Peter Hastie <phastie@bleacherreport.com>
2013-12-05 14:00:36 -0800

    Fixed files listings to allow expansion.
    Fixed indenting in Gruntfile and package.json.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,84 +1,84 @@
 /*
- * grunt-asset-packager
- * https://github.com/sillypog/grunt-asset-packager
- *
- * Copyright (c) 2013 Peter Hastie
- * Licensed under the MIT license.
- */
+* grunt-asset-packager
+* https://github.com/sillypog/grunt-asset-packager
+*
+* Copyright (c) 2013 Peter Hastie
+* Licensed under the MIT license.
+*/
 
 'use strict';
 
 module.exports = function (grunt) {
-  // load all npm grunt tasks
-  require('load-grunt-tasks')(grunt);
+	// load all npm grunt tasks
+	require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
-      }
-    },
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'<%= nodeunit.tests %>'
+			],
+			options: {
+				jshintrc: '.jshintrc',
+				reporter: require('jshint-stylish')
+			}
+		},
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-	
-	env : {
-	  dev : {
-		NODE_ENV : 'DEVELOPMENT'
-	  },
-	  prod : {
-		NODE_ENV : 'PRODUCTION'
-	  }
-	},
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp']
+		},
 
-    // Configuration to be run (and then tested).
-    asset_packager: {
-	  dev: {
-		  options: {
-			  index: 'test/fixtures/index.html',
-			  dest: 'tmp/dev'
-		  },
-		  files: {
-			  src: 'test/fixtures/asset_packages/**/*.pkg'
-		  }
-	  },
-	  prod: {
-		  options: {
-			  index: 'test/fixtures/index.html',
-			  dest: 'tmp/prod'
-		  },
-		  files: {
-			  src: 'test/fixtures/asset_packages/**/*.pkg'
-		  }
-	  }
-    },
+		env : {
+			dev : {
+				NODE_ENV : 'DEVELOPMENT'
+			},
+			prod : {
+				NODE_ENV : 'PRODUCTION'
+			}
+		},
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+		// Configuration to be run (and then tested).
+		asset_packager: {
+			dev: {
+				options: {
+					index: 'test/fixtures/index.html',
+					dest: 'tmp/dev'
+				},
+				files: [
+					{ src: ['test/fixtures/asset_packages/**/*.pkg'], expand: true }
+				]
+			},
+			prod: {
+				options: {
+					index: 'test/fixtures/index.html',
+					dest: 'tmp/prod'
+				},
+				files: [
+					{ src: ['test/fixtures/asset_packages/**/*.pkg'], expand: true }
+				]
+			}
+		},
 
-  });
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js']
+		}
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+	});
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('dev', ['env:dev', 'asset_packager:dev']);
-  grunt.registerTask('prod', ['env:prod', 'asset_packager:prod']);
-  grunt.registerTask('test', ['clean', 'dev', 'prod', 'nodeunit']);
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('dev', ['env:dev', 'asset_packager:dev']);
+	grunt.registerTask('prod', ['env:prod', 'asset_packager:prod']);
+	grunt.registerTask('test', ['clean', 'dev', 'prod', 'nodeunit']);
+
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['jshint', 'test']);
 
 };
